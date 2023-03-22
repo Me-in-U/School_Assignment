56b03b04f1a1b9e26bae244e11a4d742f2da0ee7
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-03-23 04:16:24 +0900

    Multiprogramming Simulation

---
+++ 운영체제/Assignment_04/multi.java
@@ -0,0 +1,82 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class multi {
+  public static void main(String[] args) throws IOException {
+    BufferedOutputStream bs = new BufferedOutputStream(new FileOutputStream("multi.out"));
+    BufferedReader br = new BufferedReader(new FileReader("multi.inp"));
+    int N = Integer.parseInt(br.readLine());
+    LinkedList<Deque<Integer>> programList = new LinkedList<>();
+    for (int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      Deque<Integer> processTimeData = new ArrayDeque<>();
+      processTimeData.add(0);
+      processTimeData.add(0);
+      while (st.hasMoreTokens()) {
+        processTimeData.add(Integer.parseInt(st.nextToken()));
+      }
+      programList.add(processTimeData);
+    }
+    int timeIdle = 0;
+    int timeNow = 0;
+    int programs = programList.size();
+    int processNum = 0;
+    while (processNum < programs) {
+      boolean isIdle = true;
+      Deque<Integer> processTimeData = programList.remove(processNum);
+      int isIO = processTimeData.poll();
+      int processEndTime = processTimeData.poll();
+      if (isIO == 1) {
+        if (processEndTime <= timeNow) {
+          isIO = 0;
+        } else {
+          processTimeData.addFirst(processEndTime);
+          processTimeData.addFirst(isIO);
+          programList.add(processNum, processTimeData);
+          processNum++;
+          if (isIdle && processNum == programs) {
+            timeNow++;
+            timeIdle++;
+            processNum = 0;
+          }
+          continue;
+        }
+      }
+      if (isIO == 0) {
+        int cpuTime = processTimeData.poll();
+        int ioTime = -1;
+        if (cpuTime != -1) {
+          ioTime = processTimeData.poll();
+          processEndTime = timeNow + cpuTime;
+          timeNow += cpuTime;
+        }
+        if (cpuTime != 0 && ioTime != 0 && processTimeData.size() > 1) {
+          processEndTime += ioTime;
+          isIO = 1;
+          processTimeData.addFirst(processEndTime);
+          processTimeData.addFirst(isIO);
+          programList.add(processNum, processTimeData);
+          isIdle = false;
+        } else if (ioTime != -1 && processTimeData.size() == 1 && programList.isEmpty()) {
+          timeNow += ioTime;
+          break;
+        }
+        processNum = 0;
+      }
+      programs = programList.size();
+    }
+    // System.out.println(timeIdle + " " + timeNow);
+    StringBuilder sb = new StringBuilder();
+    sb.append(timeIdle + " " + timeNow);
+    bs.write(sb.toString().getBytes());
+    bs.close();
+    br.close();
+  }
+}
